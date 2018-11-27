339af1a446623d807f00be5c60a54f52b3ab0f06
baudelaine <baudelaine@gmail.com>
2018-11-27 09:33:47 +0100

    comment l72 in getLabelsServlet

--- src/com/dma/web/GetLabelsServlet.java
+++ src/com/dma/web/GetLabelsServlet.java
@@ -69,7 +69,7 @@ public class GetLabelsServlet extends HttpServlet {
 			con = (Connection) request.getSession().getAttribute("con");
 			schema = (String) request.getSession().getAttribute("schema");
 			dbEngine = (String) request.getSession().getAttribute("dbEngine");
-			con.createStatement().execute("set schema " + schema);
+//			con.createStatement().execute("set schema " + schema);
 
 			if((dbEngine).equalsIgnoreCase("ORA")){
 				con.createStatement().execute("ALTER SESSION SET NLS_SORT=BINARY_CI");
